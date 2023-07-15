0cce235ce760524c856e5e3683f9ad1b90390faf
kayrocesar <kayro.cesar.kc@gmail.com>
2023-07-15 15:37:45 -0300

    criado end point para buscar game por id

--- src/main/java/com/example/demo/services/GameService.java
+++ src/main/java/com/example/demo/services/GameService.java
@@ -2,7 +2,9 @@ package com.example.demo.services;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.example.demo.dto.GameDTO;
 import com.example.demo.dto.GameMinDTO;
 import com.example.demo.entities.Game;
 import com.example.demo.repositories.GameRepository;
@@ -13,6 +15,13 @@ public class GameService {
 	@Autowired  // injetando uma instancia do gameRepository dentro do Game service
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true) // nao bloqueia o banco para escrita (boa pratica)
+	public GameDTO findById(Long id) { 
+		Game result =  gameRepository.findById(id).get();
+		GameDTO dto =  new GameDTO(result);
+		return dto;
+	}
+	@Transactional(readOnly = true) 
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
